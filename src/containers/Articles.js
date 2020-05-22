4ac9824d5c681e1aca064941d6d292b719ded61b
Shirley Wang <shirleywang527@gmail.com>
2020-05-22 21:44:22 +0200

    add react-spring and visibility-sensor

--- src/containers/Articles.js
+++ src/containers/Articles.js
@@ -2,11 +2,18 @@ import React from "react";
 import Articles from "../components/Articles";
 import Query from "../components/Query";
 import ARTICLE_QUERY from "../queries/articles/articles";
+import { Spring } from "react-spring/renderprops";
 
 const Home = () => {
     return (
         <main className="container container-home">
-            <h1>Shirley's Blog</h1>
+            <Spring
+                from={{ opacity: 0 }}
+                to={{ opacity: 1 }}
+                config={{ delay: 200, duration: 1000 }}
+            >
+                {(opacity) => <h1 style={opacity}>Shirley's Blog</h1>}
+            </Spring>
             <Query query={ARTICLE_QUERY}>
                 {({ data: { articles } }) => {
                     return <Articles articles={articles} />;
